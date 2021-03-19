5c3c7ef9d80ad1f630825e28a5efd06f6ba4d137
RobertVJ <robertvj@DESKTOP-IOQQ3TE.localdomain>
2021-03-19 00:00:47 -0600

    commit de prueba 1 y 2 de convolucion(El correcto es actividad.py)

--- convolucion.py
+++ convolucion.py
@@ -1,26 +1,33 @@
 import numpy as np
 
-def convolucion(IOriginal,Kernel):
-    fr=len(IOriginal)-(len(Kernel)-1)
-    cr=len(IOriginal[0])-(len(Kernel[0])-1)
+def convolucion(Ioriginal,Kernel):
+    fr= len(Ioriginal)-(len(Kernel)-1)
+    cr= len(Ioriginal[0])-(len(Kernel[0]-1))
     Resultado=np.zeros((fr,cr))
-    #Numero de elementos calculado por fila/columna
+
     #For para recorrer filas
+
     for i in range(len(Resultado)):
-        #For para recorder columnas
+        #For para recorrer columnas
         for j in range(len(Resultado[0])):
             suma=0
+            #Hace la multiplicaciones y las suma
             for m in range(len(Kernel)):
                 for n in range(len(Kernel[0])):
-                        suma+=Kernel[m][n]*IOriginal[m+i][n+j]
-                Resultado[i][j]=suma
+                        suma+=Kernel[m][n]*Ioriginal[m+i][n+j]
+            Resultado[i][j]=suma
     return Resultado
 #Imagenes
 K=[[-1,0,1],[-1,0,1],[-1,0,1]]
 I=[[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
-#Iamagenes a nuypay arrays
+
+#Imagenes a numpy arrays
 In=np.array(I)
 Kn=np.array(K)
-#Funcion de convolucion
-R=convolucion(In,Kn)
+
+# Funcion de convolucion
+
+R= convolucion(In,Kn)
 print(R)
+
+
